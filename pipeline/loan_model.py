27130c19340d35557f725a991b416d010feef2a0
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-04-12 19:46:18 -0500

    working on selecting best model

--- pipeline/loan_model.py
+++ pipeline/loan_model.py
@@ -2,6 +2,7 @@ import read
 import summarize
 import process
 import classifier
+import evaluate
 import pandas as pd
 import pylab as pl
 import numpy as np
@@ -14,9 +15,6 @@ df_train = read.read_csv('cs-training.csv', index_col = 0)
 df_test = read.read_csv('cs-training.csv', index_col = 0)
 columns = df_train.columns
 
-# x_train = df_train.copy()
-# y_train = x_train[columns[TARGET_COL]]
-# del x_train[columns[TARGET_COL]]
 
 #### create histograms for each variable in input data
 for col in columns:
@@ -38,6 +36,82 @@ for i in range(len(df_train.columns)):
         process.fill_mean(df_train, i)
         process.fill_mean(df_test, i)
 
-#### predict 
-predictions = classifier.logistic(df_train,df_test, TARGET_COL)
+
+### seperate x and y variables from training and testing sets
+x_train = df_train.copy()
+y_train = x_train[columns[TARGET_COL]]
+del x_train[columns[TARGET_COL]]
+x_new = df_test.copy()
+del x_new[columns[TARGET_COL]]
+
+
+
+###### evaluate models on training data by splitting training dataset
+logistic_model_accuracy = evaluate.evaluate_accuracy(x_train, y_train, "logistic")
+
+# ##### try alternative logistic model with some features replaced to be categorical
+# #### categorize DebtRatio and RevolvingUtilization as they had some of the most skewed distributions
+
+# df_categorical_model1 = df_train.copy()
+# process.categorize_quartile(df_categorical_model,1)
+# process.categorize_quartile(df_categorical_model,4)
+# del df_categorical_model[columns[1]]
+# del df_categorical_model[columns[4]]
+
+# x_train_categorical1 = df_categorical_model1.copy()
+# y_train_categorical1 = df_categorical_model1[columns[TARGET_COL]]
+# del x_train_categorical1[columns[TARGET_COL]]
+
+# model2_accuracy = evaluate.evaluate_accuracy(x_train_categorical, y_train_categorical, "logistic")
+
+###### add a new model with MonthlyIncome converted into a quartile variable
+
+skewness = []
+for i in range(len(columns)):
+    if i != TARGET_COL:
+        if df_train[columns[i]].min() != np.percentile(df_train[columns[i]],25) \
+        != np.percentile(df_train[columns[i]],50) != np.percentile(df_train[columns[i]],75) \
+        != df_train[columns[i]].max():
+
+            skewness.append((df_train[columns[i]].skew(),i))
+
+skewness.sort(reverse = True)
+
+
+df_train_categorical = df_train.copy()
+
+
+
+best_categorical = 0
+for skew,index in skewness:
+    process.categorize_quartile(df_train_categorical,index)
+    x_train_categorical = df_train_categorical.copy()
+    y_train_categorical = df_train_categorical[columns[TARGET_COL]]
+    del x_train_categorical[columns[TARGET_COL]]
+
+    # x_train_categorical = df_train.ix[:,df_train.columns != df_train.columns[TARGET_COL]]
+    # y_train_categorical = df_train.ix[:,df_train.columns == df_train.columns[TARGET_COL]]
+    model_accuracy = evaluate.evaluate_accuracy(x_train_categorical, y_train_categorical, "logistic")
+    if model_accuracy > best_categorical:
+        last_index = index
+        best_categorical = model_accuracy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##### train on entire training set and return predictions for testing set
+# predictions = classifier.logistic(x_train, y_train, x_new)
 
